57ed7f334ed1bf717888e857c047f5518e003694
Alexandre Oliveira <rockytvbr@gmail.com>
2017-01-19 14:54:14 -0200

    Calculate down time for server

--- app.py
+++ app.py
@@ -14,18 +14,56 @@
 #    along with status-notifier.  If not, see <http://www.gnu.org/licenses/>.
 #
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask, request
 
 import tweepy
 
 
+server_downtimes = {}
+
 def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
 
     return app
+
+def get_date_difference(a, b):
+    now = datetime.utcfromtimestamp(a)
+    other = datetime.utcfromtimestamp(b)
+
+    dt = other - now
+    total_s = dt.seconds + (dt.days * 60 * 60 * 24)
+    if total_s:
+        seconds = total_s % 60
+        total_s /= 60
+        minutes = int(total_s % 60)
+        total_s /= 60
+        hours = int(total_s % 24)
+        total_s /= 24
+        days = int(total_s)
+
+    if days > 0:
+        if hours > 0:
+            if minutes > 0:
+                return "%dd%02dh%02dm" % (days, hours, minutes)
+            else:
+                return "%dd%02dh" % (days, hours)
+        else:
+            return "%dd" % days
+    if hours > 0:
+        if minutes > 0:
+            return "%dh%02dm" % (hours, minutes)
+        else:
+            return "%dh" % hours
+    if minutes > 0:
+        if seconds > 0:
+            return "%dm%02ds" % (minutes, seconds)
+        else:
+            return "%dm" % minutes
+    else:
+        return "%02ds" % seconds
 
 # Setup our Twitter account. If any of these
 # tokens isn't defined, raise an error.
@@ -67,8 +105,17 @@ def status_webhook():
 
         if server_status == 'down':
             message = '[down] Shortage registered for %s at %s. We are working to get the server back up ASAP. Apologies!' % (server_name, date_string)
+            server_downtimes[server_name] = int(date_timestamp)
         else:
-            message = '[up] %s has resumed operation (%s), bouncers should reconnect shortly. Reach out to staff if you have any issue!' % (server_name, date_string)
+            # Check if we have a downtime for the current server.
+            # If we do, return the duration of downtime and
+            # delete the entry from the dict.
+            if server_name in server_downtimes:
+                downtime_duration = get_date_difference(server_downtimes.get(server_name), int(date_timestamp))
+                message = '[up] %s has resumed operation (%s, down for %s), bouncers should reconnect shortly.' % (server_name, date_string, downtime_duration)
+                del server_downtimes[server_name]
+            else:
+                message = '[up] %s has resumed operation (%s), bouncers should reconnect shortly.' % (server_name, date_string)
         
         api.update_status(message)
     else:
